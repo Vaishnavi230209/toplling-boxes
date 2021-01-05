03d444ac4ca2b2d9f0d83d497d396162f6e5f2c3
Vaishnavi <75157114+Vaishnavi230209@users.noreply.github.com>
2021-01-05 12:49:53 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# toplling-boxes
+# bouncyBall
+Bouncy Ball created using matter.js physics engine
---
+++ sketch.js
@@ -0,0 +1,30 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1,box2;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    
+    box1=new Box(200,300,50,100);
+    box2=new Box(170,100,50,50);
+
+    ground=new Ground(200,390,400,20);
+    
+}
+
+function draw(){
+    background("blue");
+    Engine.update(engine);
+    rectMode(CENTER);
+   
+
+   box1.display();
+   box2.display();
+   ground.display();
+}
